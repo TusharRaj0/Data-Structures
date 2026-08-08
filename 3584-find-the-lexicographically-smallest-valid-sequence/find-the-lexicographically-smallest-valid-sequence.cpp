22e173410175277ec6257297fa7ee4267f3137a0
TUSHAR RAJA <rajatushar239@gmail.com>
2026-08-08 17:45:17 +0530

    Time: 39 ms (73.40%) | Memory: 115.5 MB (29.79%) - LeetSync

---
+++ 3584-find-the-lexicographically-smallest-valid-sequence/find-the-lexicographically-smallest-valid-sequence.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<int> validSequence(string word1, string word2) {
+        int n = word1.size(), m = word2.size();
+        vector<int> right(n);
+
+        int j = m - 1, cnt = 0;
+
+        for(int i = n - 1; i >= 0; i--) {
+            right[i] = cnt;
+
+            if(j >= 0 && word1[i] == word2[j]) {
+                j--;
+                cnt++;
+            }
+        }
+
+        vector<int> ans;
+        j = 0;
+        bool changed = false;
+
+        for(int i = 0; i < n && j < m; i++) {
+            if(word1[i] == word2[j]) {
+                ans.push_back(i);
+                j++;
+            }
+            else if(!changed && right[i] >= m - 1 - j) {
+                ans.push_back(i);
+                j++;
+                changed = true;
+            }
+        }
+
+        if(j == m)
+            return ans;
+
+        return {};
+    }
+};
