50f3ec4d94def32db5fdc77c27871657c62dd1fd
TUSHAR RAJA <rajatushar239@gmail.com>
2026-08-12 18:59:32 +0530

    Time: 56 ms (45.28%) | Memory: 18.8 MB (39.88%) - LeetSync

---
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& mat, int target) {
+        int m = mat.size();
+        int n = mat[0].size();
+        int i=0,j=n-1;
+        while(i<m&&j>=0)
+        {
+            if(mat[i][j]==target)
+            {
+                return true;
+            }
+            else if(mat[i][j]<target)
+            {
+                i++;
+            }
+            else
+            {
+                j--;
+            }
+        }
+        return false;
+    }
+};
