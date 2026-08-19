fdc9953625fdd0c594a40344d716750ccfcd69d1
TUSHAR RAJA <rajatushar239@gmail.com>
2026-08-19 13:29:00 +0530

    Time: 3 ms (23.58%) | Memory: 12.9 MB (15.63%) - LeetSync

---
+++ 1047-maximize-sum-of-array-after-k-negations/maximize-sum-of-array-after-k-negations.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int largestSumAfterKNegations(vector<int>& nums, int k) {
+        priority_queue<int, vector<int>, greater<int>> pq;
+
+        for(int x : nums)
+            pq.push(x);
+
+        while(k--)
+        {
+            int x = pq.top();
+            pq.pop();
+
+            pq.push(-x);
+        }
+
+        int sum = 0;
+
+        while(!pq.empty())
+        {
+            sum += pq.top();
+            pq.pop();
+        }
+
+        return sum;
+    }
+};
